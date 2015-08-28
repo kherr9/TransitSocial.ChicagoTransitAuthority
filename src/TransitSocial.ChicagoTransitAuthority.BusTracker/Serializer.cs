d241dd45d66ed16542d40df7cfe3e4391e858863
Kevin Herr <kherr9@gmail.com>
2015-08-28 14:13:56 -0500

    add serializer file

---
+++ src/TransitSocial.ChicagoTransitAuthority.BusTracker/Serializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace TransitSocial.ChicagoTransitAuthority.BusTracker
+{
+    public class Serializer : ISerializer
+    {
+        public TModel Deserialize<TModel>(string input)
+        {
+            using (var reader = new StringReader(input))
+            using (var xmlReader = XmlReader.Create(reader))
+            {
+                var ser = new XmlSerializer(typeof(TModel));
+                return (TModel)ser.Deserialize(xmlReader);
+            }
+        }
+    }
+}
