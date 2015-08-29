e78834175d00e4e7fa276ba557fa0515697960f5
Kevin Herr <kherr9@gmail.com>
2015-08-29 10:15:11 -0500

    tweak client to support sync and async signatures

--- src/TransitSocial.ChicagoTransitAuthority.BusTracker/BusTrackerClient.cs
+++ src/TransitSocial.ChicagoTransitAuthority.BusTracker/BusTrackerClient.cs
@@ -1,15 +1,42 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TransitSocial.ChicagoTransitAuthority.BusTracker
 {
     public class BusTrackerClient : IBusTrackerClient
     {
+        private readonly string urlBase;
+
+        private readonly string key;
+
+        public BusTrackerClient(string urlBase, string key)
+        {
+            if (urlBase == null)
+            {
+                throw new ArgumentNullException("urlBase");
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            this.urlBase = urlBase;
+            this.key = key;
+        }
+
         public string GetTime()
         {
             throw new System.NotImplementedException();
         }
 
         public Task<string> GetTimeAsync()
+        {
+            return this.GetTimeAsync(CancellationToken.None);
+        }
+
+        public Task<string> GetTimeAsync(CancellationToken token)
         {
             throw new System.NotImplementedException();
         }
--- src/TransitSocial.ChicagoTransitAuthority.BusTracker/IBusTrackerClient.cs
+++ src/TransitSocial.ChicagoTransitAuthority.BusTracker/IBusTrackerClient.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TransitSocial.ChicagoTransitAuthority.BusTracker
 {
@@ -17,5 +20,13 @@ namespace TransitSocial.ChicagoTransitAuthority.BusTracker
         /// </summary>
         /// <returns></returns>
         Task<string> GetTimeAsync();
+
+        /// <summary>
+        /// Use the gettime request to retrieve the current system date and time. Since BusTime is a timedependent
+        /// system, it is important to synchronize your application with BusTime's system date and time
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        Task<string> GetTimeAsync(CancellationToken token);
     }
 }
