fec40db7fc5429b9e960c178b4bd541d0fa49b74
Kevin Herr <kherr9@gmail.com>
2015-08-30 09:10:22 -0500

    refactored tests into partials

---
+++ src/TransitSocial.ChicagoTransitAuthority.BusTracker.Tests/BusTrackerClientTests.GetTime.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TransitSocial.ChicagoTransitAuthority.BusTracker.Tests.Resources;
+
+namespace TransitSocial.ChicagoTransitAuthority.BusTracker.Tests
+{
+    public partial class BusTrackerClientTests
+    {
+        [TestMethod]
+        public void TestGetTimeInvalidApiToken()
+        {
+            // Arrange
+            const string InvalidApiToken = "INVALID_API_TOKEN";
+            var client = new BusTrackerClient(UrlBase, InvalidApiToken);
+
+            var expectedModel = this.repository.GetAs<GetTimeResponse>(ResourceFiles.GetTimeResponseInvalidApiAccess);
+
+            Exception exception = null;
+
+            // Act
+            StartOwinTest(
+                () =>
+                {
+                    try
+                    {
+                        var time = client.GetTime();
+                        Assert.Fail("Exception thrown exception");
+                    }
+                    catch (Exception ex)
+                    {
+                        exception = ex;
+                    }
+
+                    return Task.FromResult(true);
+                });
+
+            // Assert
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(expectedModel.Errors.Select(e => e.Message).First(), exception.Message);
+        }
+
+        [TestMethod]
+        public void TestGetTime()
+        {
+            // Arrange
+            var client = new BusTrackerClient(UrlBase, WebAppConfig.ApiKey);
+
+            var expectedModel = this.repository.GetAs<GetTimeResponse>(ResourceFiles.GetTimeResponse);
+
+            string time = null;
+
+            // Act
+            StartOwinTest(
+                () =>
+                {
+                    time = client.GetTime();
+
+                    return Task.FromResult(true);
+                });
+
+            // Assert
+            Assert.AreEqual(time, expectedModel.Time);
+        }
+
+    }
+}
---
+++ src/TransitSocial.ChicagoTransitAuthority.BusTracker.Tests/BusTrackerClientTests.GetTimeAsync.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TransitSocial.ChicagoTransitAuthority.BusTracker.Tests.Resources;
+
+namespace TransitSocial.ChicagoTransitAuthority.BusTracker.Tests
+{
+    public partial class BusTrackerClientTests
+    {
+        [TestMethod]
+        public void TestGetTimeAsyncInvalidApiToken()
+        {
+            // Arrange
+            const string InvalidApiToken = "INVALID_API_TOKEN";
+            var client = new BusTrackerClient(UrlBase, InvalidApiToken);
+
+            var expectedModel = this.repository.GetAs<GetTimeResponse>(ResourceFiles.GetTimeResponseInvalidApiAccess);
+
+            Exception exception = null;
+
+            // Act
+            StartOwinTest(
+                async () =>
+                {
+                    try
+                    {
+                        var time = await client.GetTimeAsync();
+                        Assert.Fail("Exception thrown exception");
+                    }
+                    catch (Exception ex)
+                    {
+                        exception = ex;
+                    }
+                });
+
+            // Assert
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(expectedModel.Errors.Select(e => e.Message).First(), exception.Message);
+        }
+
+        [TestMethod]
+        public void TestGetTimeAsyncInvalidApiTokenWithCancellationToken()
+        {
+            // Arrange
+            const string InvalidApiToken = "INVALID_API_TOKEN";
+            var client = new BusTrackerClient(UrlBase, InvalidApiToken);
+
+            var expectedModel = this.repository.GetAs<GetTimeResponse>(ResourceFiles.GetTimeResponseInvalidApiAccess);
+
+            Exception exception = null;
+
+            var cts = new CancellationTokenSource();
+
+            // Act
+            StartOwinTest(
+                async () =>
+                {
+                    try
+                    {
+                        var time = await client.GetTimeAsync(cts.Token);
+                        Assert.Fail("Exception thrown exception");
+                    }
+                    catch (Exception ex)
+                    {
+                        exception = ex;
+                    }
+                });
+
+            // Assert
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(expectedModel.Errors.Select(e => e.Message).First(), exception.Message);
+        }
+
+        [TestMethod]
+        public void TestGetTimeAsyncInvalidApiTokenWithCancelledCancellationToken()
+        {
+            // Arrange
+            const string InvalidApiToken = "INVALID_API_TOKEN";
+            var client = new BusTrackerClient(UrlBase, InvalidApiToken);
+
+            var expectedModel = this.repository.GetAs<GetTimeResponse>(ResourceFiles.GetTimeResponseInvalidApiAccess);
+
+            OperationCanceledException exception = null;
+
+            var cts = new CancellationTokenSource();
+            cts.Cancel();
+
+            // Act
+            StartOwinTest(
+                async () =>
+                {
+                    try
+                    {
+                        var time = await client.GetTimeAsync(cts.Token);
+                        Assert.Fail();
+                    }
+                    catch (OperationCanceledException ex)
+                    {
+                        exception = ex;
+                    }
+                });
+
+            // Assert
+            Assert.IsNotNull(exception);
+        }
+
+        [TestMethod]
+        public void TestGetTimeAsync()
+        {
+            // Arrange
+            var client = new BusTrackerClient(UrlBase, WebAppConfig.ApiKey);
+
+            var expectedModel = this.repository.GetAs<GetTimeResponse>(ResourceFiles.GetTimeResponse);
+
+            string time = null;
+
+            // Act
+            StartOwinTest(
+                async () =>
+                {
+                    time = await client.GetTimeAsync();
+                });
+
+            // Assert
+            Assert.AreEqual(time, expectedModel.Time);
+        }
+
+        [TestMethod]
+        public void TestGetTimeAsyncWithCancellationToken()
+        {
+            // Arrange
+            var client = new BusTrackerClient(UrlBase, WebAppConfig.ApiKey);
+
+            var expectedModel = this.repository.GetAs<GetTimeResponse>(ResourceFiles.GetTimeResponse);
+
+            var cts = new CancellationTokenSource();
+
+            string time = null;
+
+            // Act
+            StartOwinTest(
+                async () =>
+                {
+                    time = await client.GetTimeAsync(cts.Token);
+                });
+
+            // Assert
+            Assert.AreEqual(time, expectedModel.Time);
+        }
+
+        [TestMethod]
+        public void TestGetTimeAsyncWithCancelledCancellationToken()
+        {
+            // Arrange
+            var client = new BusTrackerClient(UrlBase, WebAppConfig.ApiKey);
+
+            var expectedModel = this.repository.GetAs<GetTimeResponse>(ResourceFiles.GetTimeResponse);
+
+            var cts = new CancellationTokenSource();
+            cts.Cancel();
+
+            OperationCanceledException exception = null;
+
+            // Act
+            StartOwinTest(
+                async () =>
+                {
+                    try
+                    {
+                        var time = await client.GetTimeAsync(cts.Token);
+                        Assert.Fail();
+                    }
+                    catch (OperationCanceledException ex)
+                    {
+                        exception = ex;
+                    }
+                });
+
+            // Assert
+            Assert.IsNotNull(exception);
+        }
+    }
+}
