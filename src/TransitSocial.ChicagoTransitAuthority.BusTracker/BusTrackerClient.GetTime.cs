59c71d8e29a7e8cc666cfe4585d6c1e34150d494
Kevin Herr <kherr9@gmail.com>
2015-09-01 08:07:06 -0500

    refactor async and sync files into single file

--- src/TransitSocial.ChicagoTransitAuthority.BusTracker/BusTrackerClient.GetTime.cs
+++ src/TransitSocial.ChicagoTransitAuthority.BusTracker/BusTrackerClient.GetTime.cs
@@ -34,5 +34,36 @@ namespace TransitSocial.ChicagoTransitAuthority.BusTracker
                 }
             }
         }
+
+        public Task<string> GetTimeAsync()
+        {
+            return this.GetTimeAsync(CancellationToken.None);
+        }
+
+        public async Task<string> GetTimeAsync(CancellationToken token)
+        {
+            var request = this.CreateRequest("/bustime/api/v1/gettime");
+
+            request.Method = "GET";
+
+            // will throw WebException if not success status
+            using (var response = await request.GetResponseAsync(token))
+            {
+                GetTimeResponse getTimeResponse = null;
+                using (var responseStream = response.GetResponseStream())
+                {
+                    getTimeResponse = this.Serializer.Deserialize<GetTimeResponse>(responseStream);
+                }
+
+                if (getTimeResponse.Errors != null && getTimeResponse.Errors.Any())
+                {
+                    throw new Exception(getTimeResponse.Errors.Select(x => x.Message).First());
+                }
+                else
+                {
+                    return getTimeResponse.Time;
+                }
+            }
+        }
     }
 }
--- src/TransitSocial.ChicagoTransitAuthority.BusTracker/BusTrackerClient.GetVehicles.cs
+++ src/TransitSocial.ChicagoTransitAuthority.BusTracker/BusTrackerClient.GetVehicles.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TransitSocial.ChicagoTransitAuthority.BusTracker
 {
@@ -33,7 +35,38 @@ namespace TransitSocial.ChicagoTransitAuthority.BusTracker
             }
         }
 
-        private string CreateGetVehiclesQueryString(IEnumerable<string> vehicleIds, IEnumerable<string> routeIds)
+        public Task<IEnumerable<Vehicle>> GetVehiclesAsync(IEnumerable<string> vehicleIds, IEnumerable<string> routeIds)
+        {
+            return this.GetVehiclesAsync(vehicleIds, routeIds, CancellationToken.None);
+        }
+
+        public async Task<IEnumerable<Vehicle>> GetVehiclesAsync(IEnumerable<string> vehicleIds, IEnumerable<string> routeIds, CancellationToken token)
+        {
+            var request = this.CreateRequest("/bustime/api/v1/getvehicles", this.CreateGetVehiclesQueryString(vehicleIds, routeIds));
+
+            request.Method = "GET";
+
+            // will throw WebException if not success status
+            using (var response = await request.GetResponseAsync(token))
+            {
+                GetVehiclesResponse getVehiclesResponse = null;
+                using (var responseStream = response.GetResponseStream())
+                {
+                    getVehiclesResponse = this.Serializer.Deserialize<GetVehiclesResponse>(responseStream);
+                }
+
+                if (getVehiclesResponse.Errors != null && getVehiclesResponse.Errors.Any())
+                {
+                    throw new Exception(getVehiclesResponse.Errors.Select(x => x.Message).First());
+                }
+                else
+                {
+                    return getVehiclesResponse.Vehicles;
+                }
+            }
+        }
+
+        internal string CreateGetVehiclesQueryString(IEnumerable<string> vehicleIds, IEnumerable<string> routeIds)
         {
             var temp = string.Empty;
 
