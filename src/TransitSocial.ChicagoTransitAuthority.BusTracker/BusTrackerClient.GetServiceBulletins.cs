cf1d215090b2466f3c9fe1a6357066fd6c94cf0e
Kevin Herr <kherr9@gmail.com>
2015-09-07 17:39:23 -0500

    stubbed out get service bulletins methods

---
+++ src/TransitSocial.ChicagoTransitAuthority.BusTracker/BusTrackerClient.GetServiceBulletins.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TransitSocial.ChicagoTransitAuthority.BusTracker
+{
+    public partial class BusTrackerClient
+    {
+        public IEnumerable<ServiceBulletin> GetServiceBulletins(IEnumerable<string> routeIds, string routeDirection, IEnumerable<string> stopIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ServiceBulletin>> GetServiceBulletinsAsync(IEnumerable<string> routeIds, string routeDirection, IEnumerable<string> stopIds)
+        {
+            return this.GetServiceBulletinsAsync(routeIds, routeDirection, stopIds, CancellationToken.None);
+        }
+
+        public Task<IEnumerable<ServiceBulletin>> GetServiceBulletinsAsync(IEnumerable<string> routeIds, string routeDirection, IEnumerable<string> stopIds, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
