a02438a16cefc82151567abd60d81f327b6a754b
Kevin Herr <kherr9@gmail.com>
2015-09-07 17:58:50 -0500

    add tests for building query string of service bulletins

--- src/TransitSocial.ChicagoTransitAuthority.BusTracker/BusTrackerClient.GetServiceBulletins.cs
+++ src/TransitSocial.ChicagoTransitAuthority.BusTracker/BusTrackerClient.GetServiceBulletins.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,7 +12,30 @@ namespace TransitSocial.ChicagoTransitAuthority.BusTracker
     {
         public IEnumerable<ServiceBulletin> GetServiceBulletins(IEnumerable<string> routeIds, string routeDirection, IEnumerable<string> stopIds)
         {
-            throw new NotImplementedException();
+            var request = this.CreateRequest(
+                "/bustime/api/v1/getservicebulletins",
+                CreateGetServiceBulletinsQueryString(routeIds, routeDirection, stopIds));
+
+            request.Method = "GET";
+
+            // will throw WebException if not success status
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                GetServiceBulletinResponse getServiceBulletinResponse = null;
+                using (var responseStream = response.GetResponseStream())
+                {
+                    getServiceBulletinResponse = this.Serializer.Deserialize<GetServiceBulletinResponse>(responseStream);
+                }
+
+                if (getServiceBulletinResponse.Errors != null && getServiceBulletinResponse.Errors.Any())
+                {
+                    throw new Exception(getServiceBulletinResponse.Errors.Select(x => x.Message).First());
+                }
+                else
+                {
+                    return getServiceBulletinResponse.ServiceBulletins;
+                }
+            }
         }
 
         public Task<IEnumerable<ServiceBulletin>> GetServiceBulletinsAsync(IEnumerable<string> routeIds, string routeDirection, IEnumerable<string> stopIds)
@@ -17,9 +43,67 @@ namespace TransitSocial.ChicagoTransitAuthority.BusTracker
             return this.GetServiceBulletinsAsync(routeIds, routeDirection, stopIds, CancellationToken.None);
         }
 
-        public Task<IEnumerable<ServiceBulletin>> GetServiceBulletinsAsync(IEnumerable<string> routeIds, string routeDirection, IEnumerable<string> stopIds, CancellationToken token)
+        public async Task<IEnumerable<ServiceBulletin>> GetServiceBulletinsAsync(IEnumerable<string> routeIds, string routeDirection, IEnumerable<string> stopIds, CancellationToken token)
         {
-            throw new NotImplementedException();
+            var request = this.CreateRequest(
+                "/bustime/api/v1/getservicebulletins",
+                CreateGetServiceBulletinsQueryString(routeIds, routeDirection, stopIds));
+
+            request.Method = "GET";
+
+            // will throw WebException if not success status
+            using (var response = await request.GetResponseAsync(token))
+            {
+                GetServiceBulletinResponse getServiceBulletinResponse = null;
+                using (var responseStream = response.GetResponseStream())
+                {
+                    getServiceBulletinResponse = this.Serializer.Deserialize<GetServiceBulletinResponse>(responseStream);
+                }
+
+                if (getServiceBulletinResponse.Errors != null && getServiceBulletinResponse.Errors.Any())
+                {
+                    throw new Exception(getServiceBulletinResponse.Errors.Select(x => x.Message).First());
+                }
+                else
+                {
+                    return getServiceBulletinResponse.ServiceBulletins;
+                }
+            }
+        }
+
+        internal static NameValueCollection CreateGetServiceBulletinsQueryString(
+            IEnumerable<string> routeIds,
+            string routeDirection,
+            IEnumerable<string> stopIds)
+        {
+            var queryString = CreateQueryStringCollection();
+
+            if (routeIds != null)
+            {
+                var value = string.Join(",", routeIds);
+
+                if (value != string.Empty)
+                {
+                    queryString.Add("rt", value);
+                }
+            }
+
+            if (routeDirection != null)
+            {
+                queryString.Add("rtdir", routeDirection);
+            }
+
+            if (stopIds != null)
+            {
+                var value = string.Join(",", stopIds);
+
+                if (value != string.Empty)
+                {
+                    queryString.Add("stpid", value);
+                }
+            }
+
+            return queryString;
         }
     }
 }
